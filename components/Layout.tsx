5a25828105260a1fa52a743ec41af6119afd36ff
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-07-05 21:19:52 +0900

    Add: styleを追加

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,17 @@ type Title = {
 
 const Layout: FC<Title> = ({ children, title = 'Todo App' }) => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center font-semibold text-gray-900"></div>
+    <div className="flex min-h-screen flex-col items-center justify-center font-semibold text-gray-900">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <main className="flex w-screen flex-1 flex-col items-center justify-center">
+        {children}
+      </main>
+      <footer className="flex w-full p-2 text-center">
+        <BadgeCheckIcon className="h-5 w-5 text-blue-400" />
+      </footer>
+    </div>
   )
 }
 
