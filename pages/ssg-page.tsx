d2518e9d537fe11ac1262bd4ffc2026db4874ff7
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-07-06 21:03:53 +0900

    Add: Supabaseとの連携

--- pages/ssg-page.tsx
+++ pages/ssg-page.tsx
@@ -6,12 +6,39 @@ import { Layout } from '../components/Layout'
 import { supabase } from '../utils/supabase'
 import { Task, Notice } from '../types/types'
 
-const SsgPage: NextPage = () => {
+type StaticProps = {
+  tasks: Task[]
+  notices: Notice[]
+}
+
+const SsgPage: NextPage<StaticProps> = ({ tasks, notices }) => {
   return (
-    <div>
-      <Layout title="SSG Page">{'ssg'}</Layout>
-    </div>
+    <Layout title="SSG Page">
+      <p className="mb-3 text-blue-500"></p>
+      <ul>
+        {tasks.map((task) => {
+          return <li key={task.id}>{task.title}</li>
+        })}
+      </ul>
+      <ul>
+        {notices.map((notice) => {
+          return <li key={notice.id}>{notice.content}</li>
+        })}
+      </ul>
+    </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data: tasks } = await supabase
+    .from('todos')
+    .select('*')
+    .order('created_at', { ascending: true })
+  const { data: notices } = await supabase
+    .from('notices')
+    .select('*')
+    .order('created_at', { ascending: true })
+  return { props: { tasks, notices } }
 }
 
 export default SsgPage
