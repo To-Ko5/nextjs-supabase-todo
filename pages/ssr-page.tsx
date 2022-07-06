4ecc986cfaa628348d701a15e3dfaba6aaea18d7
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-07-06 21:13:01 +0900

    Add: Supabaseとの連携

--- pages/ssr-page.tsx
+++ pages/ssr-page.tsx
@@ -1,7 +1,44 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { NextPage } from 'next'
+import { GetServerSideProps } from 'next'
+import { Layout } from '../components/Layout'
+import { supabase } from '../utils/supabase'
+import { Task, Notice } from '../types/types'
 
-const SsrPage: NextPage = () => {
-  return <></>
+type StaticProps = {
+  tasks: Task[]
+  notices: Notice[]
+}
+
+const SsrPage: NextPage<StaticProps> = ({ tasks, notices }) => {
+  return (
+    <Layout title="SSG Page">
+      <p className="mb-3 text-blue-500"></p>
+      <ul>
+        {tasks.map((task) => {
+          return <li key={task.id}>{task.title}</li>
+        })}
+      </ul>
+      <ul>
+        {notices.map((notice) => {
+          return <li key={notice.id}>{notice.content}</li>
+        })}
+      </ul>
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetServerSideProps = async () => {
+  const { data: tasks } = await supabase
+    .from('todos')
+    .select('*')
+    .order('created_at', { ascending: true })
+  const { data: notices } = await supabase
+    .from('notices')
+    .select('*')
+    .order('created_at', { ascending: true })
+  return { props: { tasks, notices } }
 }
 
 export default SsrPage
