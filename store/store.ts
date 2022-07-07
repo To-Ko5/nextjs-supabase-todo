d6cab7edef17349f4629bc90e7bdb351e3474245
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-07-07 18:53:29 +0900

    Add: zustand methods

--- store/store.ts
+++ store/store.ts
@@ -1,3 +1,4 @@
+import { userInfo } from 'os'
 import create from 'zustand'
 import { EditedTask, EditedNotice } from '../types/types'
 
@@ -9,3 +10,26 @@ type State = {
   resetEditedTask: () => void
   resetEditedNotice: () => void
 }
+
+const useStore = create<State>((set) => ({
+  editedTask: { id: '', title: '' },
+  editedNotice: { id: '', content: '' },
+  updateEditedTask: (payload) =>
+    set({
+      editedTask: {
+        id: payload.id,
+        title: payload.title
+      }
+    }),
+  resetEditedTask: () => set({ editedTask: { id: '', title: '' } }),
+  updateEditedNotice: (payload) =>
+    set({
+      editedNotice: {
+        id: payload.id,
+        content: payload.content
+      }
+    }),
+  resetEditedNotice: () => set({ editedNotice: { id: '', content: '' } })
+}))
+
+export default useStore
