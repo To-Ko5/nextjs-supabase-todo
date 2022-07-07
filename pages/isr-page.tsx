b8b2ee98b8adffe19fb0284fac583cf2b35432ae
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-07-07 10:30:22 +0900

    Add: Supabaseとの連携

--- pages/isr-page.tsx
+++ pages/isr-page.tsx
@@ -1,5 +1,45 @@
-const IsrPage = () => {
-  return <div>isr-page</div>
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { NextPage } from 'next'
+import { GetStaticProps } from 'next'
+import { Layout } from '../components/Layout'
+import { supabase } from '../utils/supabase'
+import { Task, Notice } from '../types/types'
+
+type StaticProps = {
+  tasks: Task[]
+  notices: Notice[]
+}
+
+const IsrPage: NextPage<StaticProps> = ({ tasks, notices }) => {
+  const router = useRouter()
+  return (
+    <Layout title="ISR Page">
+      <p className="mb-3 text-blue-500">ISR</p>
+      <ul>
+        {tasks.map((task) => {
+          return <li key={task.id}>{task.title}</li>
+        })}
+      </ul>
+      <ul>
+        {notices.map((notice) => {
+          return <li key={notice.id}>{notice.content}</li>
+        })}
+      </ul>
+    </Layout>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data: tasks } = await supabase
+    .from('todos')
+    .select('*')
+    .order('created_at', { ascending: true })
+  const { data: notices } = await supabase
+    .from('notices')
+    .select('*')
+    .order('created_at', { ascending: true })
+  return { props: { tasks, notices }, revalidate: 5 } // ISRの時はrevalidateを指定
 }
 
 export default IsrPage
