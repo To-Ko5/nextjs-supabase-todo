84a82d2bd70dfe56327613da631b26ea53767bd1
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-07-07 16:53:06 +0900

    Add: dashboard html

--- pages/todo/dashboard.tsx
+++ pages/todo/dashboard.tsx
@@ -5,9 +5,33 @@ import { GetStaticProps } from 'next'
 import { Layout } from '../../components/Layout'
 import { supabase } from '../../utils/supabase'
 import { Task, Notice } from '../../types/types'
+import {
+  LogoutIcon,
+  StatusOnlineIcon,
+  DocumentTextIcon
+} from '@heroicons/react/solid'
 
 const Dashboard: NextPage = () => {
-  return <div>Dashboard</div>
+  const signOut = () => {
+    supabase.auth.signOut()
+  }
+
+  return (
+    <Layout title="dashboard">
+      <div className="grid grid-cols-2 gap-40">
+        <div>
+          <div className="my-3 flex justify-center">
+            <DocumentTextIcon className=" h-4 w-4 " />
+          </div>
+        </div>
+        <div>
+          <div className="my-3 flex justify-center ">
+            <StatusOnlineIcon className=" h-4 w-4 " />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
 }
 
 export default Dashboard
