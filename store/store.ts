abb7a437deb14cc6ca9f01cc2cf7c3e949d999fd
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-07-07 18:43:21 +0900

    Add: store zustand

---
+++ store/store.ts
@@ -0,0 +1,11 @@
+import create from 'zustand'
+import { EditedTask, EditedNotice } from '../types/types'
+
+type State = {
+  editedTask: EditedTask
+  editedNotice: EditedNotice
+  updateEditedTask: (payload: EditedTask) => void
+  updateEditedNotice: (payload: EditedNotice) => void
+  resetEditedTask: () => void
+  resetEditedNotice: () => void
+}
